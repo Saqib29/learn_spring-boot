0ac30c881e2f86a88a0bca4b1eca667592448c9a
Saqib <saqib29111@gmail.com>
2021-08-21 13:02:39 +0600

    Saving Students

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student saqib = new Student(
+                    "Saqib",
+                    "saqib29111@gmail.com",
+                    LocalDate.of(1995, Month.NOVEMBER, 29),
+                    26
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(1995, Month.JANUARY, 29),
+                    26
+            );
+
+            repository.saveAll(
+                    List.of(saqib, alex)
+            );
+        };
+    }
+}
