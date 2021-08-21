4af2751f0774ab983084f7357e1a5a4daa097124
Saqib <saqib29111@gmail.com>
2021-08-21 15:58:09 +0600

    PostMapping

--- demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -16,15 +16,13 @@ public class StudentConfig {
             Student saqib = new Student(
                     "Saqib",
                     "saqib29111@gmail.com",
-                    LocalDate.of(1995, Month.NOVEMBER, 29),
-                    26
+                    LocalDate.of(1995, Month.NOVEMBER, 29)
             );
 
             Student alex = new Student(
                     "Alex",
                     "alex@gmail.com",
-                    LocalDate.of(1995, Month.JANUARY, 29),
-                    26
+                    LocalDate.of(1995, Month.JANUARY, 29)
             );
 
             repository.saveAll(
