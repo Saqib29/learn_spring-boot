6fbc1385d554104b55f2f66234ea26b2e61ef087
Saqib <saqib29111@gmail.com>
2021-08-30 16:14:37 +0600

    interface and class to use it

---
+++ crud/src/main/java/lexoro/ai/crud/dao/PersonDao.java
@@ -0,0 +1,14 @@
+package lexoro.ai.crud.dao;
+
+import lexoro.ai.crud.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
