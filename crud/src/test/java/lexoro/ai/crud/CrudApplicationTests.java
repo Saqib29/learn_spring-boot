729f315070cc67353042ff3de125f6ede372b5cd
Saqib <saqib29111@gmail.com>
2021-08-30 11:59:15 +0600

    new fresh project of CRUD in spring boot

---
+++ crud/src/test/java/lexoro/ai/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package lexoro.ai.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
