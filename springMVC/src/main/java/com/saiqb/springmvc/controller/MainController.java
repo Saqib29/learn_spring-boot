234fca22dc0e5529f3ee6f22e54a9ed8b49bbdac
Saqib <saqib29111@gmail.com>
2021-09-01 18:18:26 +0600

    @GetMapping details

--- springMVC/src/main/java/com/saiqb/springmvc/controller/MainController.java
+++ springMVC/src/main/java/com/saiqb/springmvc/controller/MainController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @ControllerAdvice
 public class MainController {
@@ -24,5 +27,16 @@ public class MainController {
     public String addProgrammer(@ModelAttribute Programmer programmer){
 
         return "ProgrammerInfo.html";
+    }
+
+    @GetMapping("/allProgrammer")
+    public String allProgrammer(Model m){
+        List<Programmer> p = new ArrayList<Programmer>();
+        p.add(new Programmer(101, "Zabir Rahman", "Java"));
+        p.add(new Programmer(102, "Saqib Aminul", "Python"));
+        p.add(new Programmer(103, "Aminul Islam Saqib", "JavaScript"));
+
+        m.addAttribute("programmers", p);
+        return "AllProgrammer.html";
     }
 }
