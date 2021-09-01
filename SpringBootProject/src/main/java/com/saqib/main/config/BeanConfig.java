cbf8b643bf67d5feaa5b338d48c2912f7bf8f547
Saqib <saqib29111@gmail.com>
2021-09-01 12:23:44 +0600

    Java Config Based Constructor Injection

--- SpringBootProject/src/main/java/com/saqib/main/config/BeanConfig.java
+++ SpringBootProject/src/main/java/com/saqib/main/config/BeanConfig.java
@@ -11,37 +11,30 @@ public class BeanConfig {
 
     @Bean
     public Coder coder1(@Qualifier("computer1") Computer computer){
-        Coder c1 = new Coder();
+        Coder c1 = new Coder(1001, "MD Adam", "Java", computer);
         c1.setId(1001);
-        c1.setName("Md Adam");
-        c1.setLanguage("Java");
-        c1.setComputer(computer);
 
         return c1;
     }
 
     @Bean
     public Computer computer1(){
-        Computer com1 = new Computer();
-        com1.setBrand("AMD");
+        Computer com1 = new Computer("AMD");
+
         return com1;
     }
 
     @Bean
     public Coder coder2(@Qualifier("computer2") Computer computer){
-        Coder c1 = new Coder();
-        c1.setId(1001);
-        c1.setName("Mr Eve");
-        c1.setLanguage("JavaScript");
-        c1.setComputer(computer);
+        Coder c1 = new Coder(1002, "Mr Eve", "JavaScript", computer);
 
         return c1;
     }
 
     @Bean
     public Computer computer2(){
-        Computer com1 = new Computer();
-        com1.setBrand("Intel");
+        Computer com1 = new Computer("Intel");
+
         return com1;
     }
 }
