30f68bd7d4a76bc34aab5b6d6f5a5be320ee7b5f
Saqib <saqib29111@gmail.com>
2021-09-01 12:33:59 +0600

    new fresh project Autowire

---
+++ AutowiringExample/src/main/java/com/Saqib/AutowiringExample/AutowiringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.Saqib.AutowiringExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowiringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutowiringExampleApplication.class, args);
+	}
+
+}
---
+++ AutowiringExample/src/test/java/com/Saqib/AutowiringExample/AutowiringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Saqib.AutowiringExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutowiringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
