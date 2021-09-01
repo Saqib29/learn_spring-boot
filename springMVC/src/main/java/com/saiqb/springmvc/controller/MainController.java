ad76110c7e3e53297b3a8ba85a1a6d3b7d5e4d7a
Saqib <saqib29111@gmail.com>
2021-09-01 17:34:34 +0600

    Method Level @ModelAttribute

--- springMVC/src/main/java/com/saiqb/springmvc/controller/MainController.java
+++ springMVC/src/main/java/com/saiqb/springmvc/controller/MainController.java
@@ -3,13 +3,20 @@ package com.saiqb.springmvc.controller;
 import com.saiqb.springmvc.model.Programmer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@ControllerAdvice
 public class MainController {
+
+    @ModelAttribute
+    public void welcome(Model m){
+        m.addAttribute("msg", "Welcome to Spring Boot Tutorial");
+    }
 
     @RequestMapping("/home")
     public String homePage(){
