67a308991756bcbe2ded8f683110913777e69f19
Saqib <saqib29111@gmail.com>
2021-09-01 16:46:56 +0600

    take input, show data via thymleaf -> Model & ModelMap

--- springMVC/src/main/java/com/saiqb/springmvc/controller/MainController.java
+++ springMVC/src/main/java/com/saiqb/springmvc/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.saiqb.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -9,5 +11,18 @@ public class MainController {
     @RequestMapping("/home")
     public String homePage(){
         return "HomePage.html";
+    }
+
+    @RequestMapping("/addProgrammer")
+    public String addProgrammer(@RequestParam int pId,
+                                @RequestParam String pName,
+                                @RequestParam String pLang,
+                                Model model){
+
+        model.addAttribute("pName", pName);
+        model.addAttribute("pId", pId);
+        model.addAttribute("pLang", pLang);
+
+        return "ProgrammerInfo.html";
     }
 }
