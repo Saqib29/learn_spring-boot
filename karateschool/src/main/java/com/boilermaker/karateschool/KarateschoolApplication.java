4c12d373af04bc8925a2a8a6dc5a1237f5e0dce3
Saqib <saqib29111@gmail.com>
2021-10-18 12:47:07 +0600

    new project build spring boot API connected to postgresql

---
+++ karateschool/src/main/java/com/boilermaker/karateschool/KarateschoolApplication.java
@@ -0,0 +1,13 @@
+package com.boilermaker.karateschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarateschoolApplication.class, args);
+	}
+
+}
---
+++ karateschool/src/test/java/com/boilermaker/karateschool/KarateschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boilermaker.karateschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarateschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
